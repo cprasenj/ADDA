f1eca8b1e249a1f7b03aa6d1292d2de0a9f0e3fd
Prasenjit Chakraborty <cprasenj@thoughtworks.com>
2014-12-17 18:31:38 +0530

    Initialization of Database

---
+++ scripts/initializeDB.js
@@ -0,0 +1,24 @@
+var location = process.argv[2];
+var sqlite3 = require("sqlite3");
+var db = new sqlite3.Database(location);
+var runAllQueries = function(){	
+	var runQuery = function(q){
+		console.log(q);
+		db.run(q,function(err){
+			if(err){
+				console.log(err);
+				process.exit(1);
+			}
+		});
+	};
+
+	[	"create table users(emailId text primary key, name text not null, secret text not null);",
+		"create table topics(id integer primary key autoincrement, name text not null,"+
+			" description text, ownersEmailId text not null, startTime text not null, closeTime text,"+
+			" foreign key(ownersEmailId) references users(emailId));",
+		"create table comments(id integer primary key autoincrement, topicId integer not null,"+
+			"emailId text not null, comment text not null, time text not null,"+
+			" foreign key(topicId) references topics(id), foreign key(emailId) references users(emailId));"
+	].forEach(runQuery)	;
+};
+db.serialize(runAllQueries);
