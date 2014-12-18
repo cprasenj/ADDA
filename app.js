e0d61a738395e76a71cde14e3e1b5a533e7ca13b
Prasenjit Chakraborty <cprasenj@thoughtworks.com>
2014-12-18 10:33:41 +0530

    changed app.js

--- app.js
+++ app.js
@@ -4,10 +4,9 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
 var routes = require('./routes/index');
 var users = require('./routes/users');
-
+var school_records = require('./own_modules/school_records').init('./data/school.db');
 var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -19,8 +18,8 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', routes);
 app.use('/users', users);
 
