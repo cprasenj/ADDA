2a29ea78bd28f26c98542a054ecc0d4f09423571
Prasenjit Chakraborty <cprasenj@thoughtworks.com>
2014-12-19 10:22:31 +0530

    users object is deleted from db.json

--- routes/pages.js
+++ routes/pages.js
@@ -1,32 +1,23 @@
 var express = require('express');
-var lib = require('../library/userStore.js').create();
 var router = express.Router();
 var records = require("../ownModules/addaRecords.js").create("./data/addaDB.json",0);
 module.exports = router;
 
 router.get('/', function(req, res) {
-    console.log(records.db.topics);
-  res.render('index',{titlt:'adda',topics:records.db[5]});
+	var topics = records.getTop5Topics();
+	res.render('index', { title:'Home',topics:topics});
 });
-
-router.get('/registration',function(req,res) {
-    res.render('registration');
+router.get('/index', function(req, res) {
+	var topics = records.getTop5Topics();
+	res.render('index', { title:'Home',topics:topics});
 });
-
-router.post('/registration',function(req,res) {
-    var result = lib.save({
-    firstName:req.body.firstName,
-    lastName:req.body.lastName,
-    email:req.body.email,
-    password:req.body.password
-  });
-  result.error ? res.render('registration',result) : res.redirect('/dashboard');  
-
+router.get('/index.html', function(req, res) {
+	var topics = records.getTop5Topics();
+	res.render('index', { title:'Home',topics:topics});
 });
 
 router.get('/topic/:id',function(req,res) {
 	var id = req.params.id;
-    console.log(records.db);
 	var topic = records.db['topics'][id];
 	topic['id'] = id;
 	topic['comments'] = records.loadRecentComments(id);
@@ -50,7 +41,7 @@ router.get("/topics",function(req,res){
 	res.render('topics',{title:'Topics'})
 });
 
-router.post("/addTopic",function(req,res){
+router.post("/topicAdd",function(req,res){
 	var email = "mahesh@mail.com"; 
 	var topicName = req.body.topicName;
 	var topicDescription = req.body.topicDescription;
@@ -62,16 +53,7 @@ router.get('/login', function(req, res) {
 	res.render('login',{title:'Login'});
 });
 
-router.post('/validate',function(req,res,next){
+router.post('/validate',function(req,res){
 	var validity = records.validate(req.body);
 	(validity)? res.redirect('/dashboard') : res.redirect('/login');
 });
-
-router.get('/logout',function(req,res){
-    req.session.destroy();
-    res.redirect('/');
-});
-
-
-
-
