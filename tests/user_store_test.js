816bb131350aaed4b3ce13bc06be78fd84d86550
Prasenjit Chakraborty <cprasenj@thoughtworks.com>
2014-12-22 15:08:26 +0530

    user_store_tests are passing

--- tests/user_store_test.js
+++ tests/user_store_test.js
@@ -4,18 +4,18 @@ var userStore = require("../library/userStore.js").create("./data/db.json");
 
 describe("userStore", function(){
 	describe("load",function(){
-		// it("should load the user of given email id",function(done){
-		// 	assert.deepEqual(userStore.load("mahesh@mail.com"),{ email: 'mahesh@mail.com',
-  // 				name: 'Mahesh Kumar',
-  // 				password: 'mahesh' });
-		// 	done();
-		// });
-		// it("should load the user of given email id budda@mail.com",function(done){
-		// 	assert.deepEqual(userStore.load("budda@mail.com"),{ email: 'budda@mail.com',
-  // 				name: 'Buddarthan A N',
-  // 				password: 'secret' });
-		// 	done();
-		// });
+		it("should load the user of given email id",function(done){
+			assert.deepEqual(userStore.load("mahesh@mail.com"),{ email: 'mahesh@mail.com',
+  				name: 'Mahesh Kumar',
+  				password: 'mahesh' });
+			done();
+		});
+		it("should load the user of given email id budda@mail.com",function(done){
+			assert.deepEqual(userStore.load("budda@mail.com"),{ email: 'budda@mail.com',
+  				name: 'Buddarthan A N',
+  				password: 'secret' });
+			done();
+		});
 	});
 
 });
