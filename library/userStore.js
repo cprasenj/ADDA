de3b3489ca16a129f25f2ee9944350afc1d7e7b6
Prasenjit Chakraborty <cprasenj@thoughtworks.com>
2014-12-22 15:31:19 +0530

    tests commented

--- library/userStore.js
+++ library/userStore.js
@@ -1,9 +1,10 @@
 var fs = require('fs');
 var _ = require('lodash');
-var USERFILE = './data/addaDB.json';
+var USERFILE = './tests/data/db.json';
 var db = fs.existsSync(USERFILE)?
 	JSON.parse(fs.readFileSync(USERFILE,'utf-8')):
 	[];
+	console.log(db);
 var saveAll = function(USERFILE){
 	fs.writeFile(USERFILE,JSON.stringify(db));
 };
@@ -18,7 +19,7 @@ exports.create = function(location){
 			return {};
 		},
 		load: function(email){
-			return _.find(db[0].users,{email:email});
+			return _.find(db[0]["loginDetails"],{email:email});
 		}
 	};
 };
