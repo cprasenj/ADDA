4865c3ba7a840743a957c5de339235560357b950
Prasenjit Chakraborty <cprasenj@thoughtworks.com>
2014-12-27 11:37:58 +0530

    1) Session timeout is extended to 24 hrs. 2) Login page validation is solved 3) create table (joinTopic) query is removed from initializeDB.js

--- app.js
+++ app.js
@@ -11,23 +11,14 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(session({name:"Adda",secret: "This is Adda App secret string", cookie:{maxAge:600000}}));
+app.use(session({name:"Adda",secret: "This is Adda App secret string",cookie:{maxAge: Date.now() + 24 * 3600000}}));
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', pages);
-// app.use('/topic/:id',pages);
-// app.get('/registration',pages);
-// app.post('/registration',pages);
-// app.post('/topic/:id/addComment',pages);
-// app.post('/validate',pages);
-// app.post('/searchTopic',pages);
-
 // catch 404 and forward to error handler
 
 app.use(function(req, res, next) {
